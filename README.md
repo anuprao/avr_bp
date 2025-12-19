f811a72f4a1cdb7c3b3fd5956ea09df0aff5a874
Anup Jayapal Rao <anup.kadam@gmail.com>
2025-12-19 10:25:10 +0530

    Updated the commands to build, compile and hexlify

---
+++ README.md
@@ -0,0 +1,5 @@
+avr-gcc boilerplate with makefile
+
+All copyrighted stuff belongs to their respective owners.
+Content presented here for educational purposes.
+
--- src/main.c
+++ src/main.c
@@ -1,26 +1,42 @@
-#include <avr/io.h>
+// ~/apps/avrdude/bin/avrdude ~/apps/avrdude/etc/avrdude.conf -F -v -V -pattiny2313 -cavrisp -P/dev/ttyUSB0 -b19200 -U lfuse:r:-:h -U hfuse:r:-:h -U efuse:r:-:h
+// ~/apps/avrdude/bin/avrdude ~/apps/avrdude/etc/avrdude.conf -F -v -V -pattiny2313 -cavrisp -P/dev/ttyUSB0 -b19200 -U flash:w:build/demo.hex
+// ~/apps/avrdude/bin/avrdude ~/apps/avrdude/etc/avrdude.conf -F -v -V -pattiny2313 -cavrisp -P/dev/ttyUSB0 -b19200 -U flash:r:build/verify.hex
+//
 
-void Delay(void)
-{
-	volatile unsigned int del = 40000;
-	
-	while(del--);
-}
+#include <avr/io.h>
 
 int main(void)
 {
-	// LEDs are on pins PD0 and PD5
-	DDRD |= ((1 << PD0) | (1 << PD5));
-	
-	while(1)
-	{
-		PORTD &= ~(1 << PD0);			// switch PD0 LED off
-		PORTD |=  (1 << PD5);			// switch PD5 LED on
-		Delay();
-		PORTD &= ~(1 << PD5);			// switch PD5 LED off
-		PORTD |=  (1 << PD0);			// switch PD0 LED on
-		Delay();
-	}		
+    char toggle = 0;        // toggle LED flag
+    
+    DDRA |= (1 << PA0);     // LED on PA0
+
+    OCR1A = 0x4000;         // number to count to
+    TCCR1A = 0;             // CTC mode
+    
+    // CTC mode, clk src = clk/8, start timer
+    TCCR1B = (1 << WGM12) | (1 << CS11);
+    
+    while(1)
+    {
+	    // count reached?
+        if (TIFR & (1 << OCF1A)) 
+        {  
+            TIFR |= (1 << OCF1A);   // clear flag
+            
+            if (toggle) 
+            {   
+            	// toggle LED
+                toggle = 0;
+                PORTA &= ~(1 << PA0);
+            }
+            else 
+            {
+                toggle = 1;
+                PORTA |=  (1 << PA0);
+            }
+        }            
+    }	
 }
 
 
